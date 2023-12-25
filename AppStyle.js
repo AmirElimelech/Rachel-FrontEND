0c7e8cec132860666262f32cfa51e6f3a596afb2
AmirElimelech <sapinhopreto@gmail.com>
2023-12-25 02:08:31 +0200

    Added style for main app to add background not complete yet , and added a temp background pic > will be replaced with something related to our need of app

---
+++ AppStyle.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover', // or 'stretch'
+    width: '100%', // Ensure it covers the full width of the screen
+    height: '100%' // Ensure it covers the full height of the screen
+  },
+});
