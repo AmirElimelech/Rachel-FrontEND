daf59dfd51f9e7d7eb6474f50274718b473266dc
AmirElimelech <sapinhopreto@gmail.com>
2023-12-25 02:04:13 +0200

    Added components into the main app.js

--- App.js
+++ App.js
@@ -1,8 +1,22 @@
+
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './components/LoginScreen';
+import SignupScreen from './components/SignupScreen';
+
+const Stack = createStackNavigator();
 
 const App = () => {
-  return <LoginScreen />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Signup" component={SignupScreen} />
+        {/* Add other screens here as needed */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
